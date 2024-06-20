62ac324b30e56b4d698c3d18ac63d9799c0664d0
Kapil Sharma <ks3913688@gmail.com>
2024-06-21 01:00:34 +0530

    Update modify_syscalls.py

--- modify_syscalls.py
+++ modify_syscalls.py
@@ -4,36 +4,73 @@ import ctypes as ct
 prog = """
 #include <uapi/linux/ptrace.h>
 #include <linux/sched.h>
-#define SIGTERM 15
+
+#define SIGKILL 9
+
 struct key_t {
     char fname[NAME_MAX];
 };
-BPF_HASH(secure_files, struct key_t, int);
+
+struct auth_key_t {
+    u32 uid;
+};
+
+struct auth_value_t {
+    int access_level;
+};
+
+BPF_HASH(protected_files, struct key_t, int);
+BPF_HASH(authorized_users, struct auth_key_t, struct auth_value_t);
+
 int syscall__openat(struct pt_regs *ctx, int dfd, const char __user *filename, int flags) {
     struct key_t key = {};
+    struct auth_key_t auth_key = {};
+    struct auth_value_t *auth_value;
+    
     u32 uid = bpf_get_current_uid_gid();
+    auth_key.uid = uid;
+    
     bpf_probe_read_user_str(&key.fname, sizeof(key.fname), (void *)filename);
-    int *security_level = secure_files.lookup(&key);
-    if (security_level != 0) {
+    int *protection_level = protected_files.lookup(&key);
+    
+    if (protection_level != 0) {
+        auth_value = authorized_users.lookup(&auth_key);
+        
+        if (auth_value) {
+            if (auth_value->access_level >= *protection_level) {
+                bpf_trace_printk("Authorized user (UID: %d) opening protected file %s\\n", uid, key.fname);
+                return 0;
+            }
+        }
+        
         if (uid == 0) {
-            bpf_trace_printk("Root user opening secret file %s \\n", key.fname);
+            bpf_trace_printk("Root user opening protected file %s\\n", key.fname);
             return 0;
         }
-        bpf_trace_printk("Non-root user attempt to open secure file %s with security level %d \\n", key.fname, *security_level );
-        if (*security_level == 1) {
+        
+        bpf_trace_printk("Unauthorized user (UID: %d) attempting to open protected file %s with protection level %d\\n", uid, key.fname, *protection_level);
+        
+        if (*protection_level == 1) {
             bpf_override_return(ctx, -EACCES);
-        } else if (*security_level > 1) {
-            bpf_send_signal(SIGTERM);
+        } else if (*protection_level > 1) {
+            bpf_send_signal(SIGKILL);
         }
     }
     return 0;
 }
 """
 
-def add_file(map, file, security_level):
+def add_file(map, file, protection_level):
     key = map.Key()
     key.fname = file.encode()
-    value = ct.c_int(security_level)
+    value = ct.c_int(protection_level)
+    map[key] = value
+
+def add_authorized_user(map, uid, access_level):
+    key = map.Key()
+    key.uid = uid
+    value = map.Leaf()
+    value.access_level = access_level
     map[key] = value
 
 def main():
@@ -41,16 +78,23 @@ def main():
     fnname_openat = b.get_syscall_prefix().decode() + 'openat'
     b.attach_kprobe(event=fnname_openat, fn_name="syscall__openat")
     
-    secure_files = b.get_table("secure_files")
+    protected_files = b.get_table("protected_files")
+    authorized_users = b.get_table("authorized_users")
     
-    # Read secure files from secure_files.txt
-    with open('secure_files.txt', 'r') as f:
+    # Read secret files from protected_files.txt
+    with open('protected_files.txt', 'r') as f:
         for line in f:
-            file, security_level = line.strip().split(',')
-            add_file(secure_files, file, int(security_level))
+            file, protection_level = line.strip().split(',')
+            add_file(protected_files, file, int(protection_level))
+    
+    # Read authorized users from authorized_users.txt
+    with open('authorized_users.txt', 'r') as f:
+        for line in f:
+            uid, access_level = line.strip().split(',')
+            add_authorized_user(authorized_users, int(uid), int(access_level))
     
     try:
-        print("Attaching kprobe to sys_openat... Press Ctrl+C to exit.")
+        print("Hi, I will protect your files now :)")
         b.trace_print()
     except KeyboardInterrupt:
         pass
